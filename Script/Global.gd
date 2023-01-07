bf60581fd9a1947cf98fd2e7d5d2c788fb2030c2
Sam <lemonverse@gmail.com>
2023-01-07 14:52:24 +1300

    menu

--- Script/Global.gd
+++ Script/Global.gd
@@ -3,6 +3,9 @@ extends Node
 var ACTIVE_LIGHTS := [];
 var ACTIVE_INTERACTABLES := [];
 
+var MUSIC := true;
+var SOUND := true;
+
 var UI : UI;
 var OBJECTS : Node2D;
 var PLAYER : Player;
---
+++ Script/MainMenu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:	
+	if(OS.get_name() == "Web"):
+		$PanelContainer/VBoxContainer/Quit.queue_free();
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_packed(load("res://Scene/LevelTest.tscn"))
+
+
+func _on_game_sounds_pressed() -> void:
+	Global.SOUND = $PanelContainer/VBoxContainer/GameSounds.button_pressed;
+
+func _on_music_pressed() -> void:
+	Global.MUSIC = $PanelContainer/VBoxContainer/Music.button_pressed;
+
+func _on_quit_pressed() -> void:
+	get_tree().quit();
